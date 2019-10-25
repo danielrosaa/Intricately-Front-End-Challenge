666a6cf76f6ce2eb03d9a7fce4035747849c6d5a
Daniel <drosa@indracompany.com>
2019-10-25 15:29:01 -0300

    Creates footer.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -49,6 +49,26 @@
       </div>
 
     </div>
+    <footer>
+      <div class="footer-content">
+        <div class="row">
+          <ul>
+            <li v-for="item in footerLinks" :key="item.name">{{item.name}}</li>
+          </ul>
+          <ul>
+            <li v-for="item in legalInfos" :key="item.name">{{item.name}}</li>
+          </ul>
+        </div>
+        <div class="row">
+          <div>&copy; Copyright 2019, Intricately, Inc. All Rights Reserved.</div>
+          <div>
+            <img src="/assets/images/Company Page/fill-118.png" alt="LinkedIn Icon">
+            <img src="/assets/images/Company Page/fill-92.png" alt="Facebook Icon">
+            <img src="/assets/images/Company Page/fill-93.png" alt="Twiiter Icon">
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -61,6 +81,17 @@ export default {
         {name: 'COMPANY DATA', route: '/companydata'},
         {name: 'COMPANY TABLE', route: ''},
         {name: 'COMPANY PAGE', route: '/companypage'}
+      ],
+      footerLinks: [
+        {name: 'Pricing'},
+        {name: 'Blog'},
+        {name: 'Webinars'},
+        {name: 'Events'},
+        {name: 'Help Center'},
+      ],
+      legalInfos: [
+        {name: 'Terms and Conditions'},
+        {name: 'Privacy Policy'},
       ]
     }
   }
